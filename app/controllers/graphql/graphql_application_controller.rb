d1eb21b23477dae47a25ba5eeb84ab3d3385dffc
Mikhail Kilin <mikhail.kilin@flatstack.com>
2021-01-31 18:47:45 +0300

    fixes

--- app/controllers/graphql/graphql_application_controller.rb
+++ app/controllers/graphql/graphql_application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module Graphql
-  # base class of all GraphqlControllers
   class GraphqlApplicationController < GraphqlRails::Controller
-    # TODO: add app specific customizations here
   end
 end
